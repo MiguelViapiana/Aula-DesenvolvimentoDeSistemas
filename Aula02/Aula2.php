2b1f7e5c56fa5cbc49feada835cfaf4a88b940fb
Miguel Viapiana Jung <127536583+MiguelViapiana@users.noreply.github.com>
2024-03-05 09:17:16 -0300

    Create Aula2.php

---
+++ Aula02/Aula2.php
@@ -0,0 +1,17 @@
+<?php 
+
+    echo "<h1>Olá Mundo</h1>";
+    $num =3;
+    $texto = "String elgal";
+    $float = 5.7;
+    $bool = false;
+
+    $nome = "jose";
+    $idade = 87;
+    $faculdade = "ADS";
+
+    echo "Nome: ". $nome . "<br>";
+    echo "Idade: ". $idade . "<br>";
+    echo "Faculdade: ". $faculdade;
+
+?> 
